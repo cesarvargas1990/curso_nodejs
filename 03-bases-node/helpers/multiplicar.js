b2a677f615760fec4d8122612af5265687d513af
cesar vargas <cesara.vargas1990@gmail.com>
2023-09-29 15:16:21 -0500

    ajuste problema de consola

--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -2,14 +2,16 @@
 const tabla = async (m,l,s) => {
     tit = '';
     acum = '';
+    acumc = '';
     tit = tit+"=================\n"
     tit = tit+"   TABLA DEL "+m+"\n"
     tit = tit+ "=================\n"
     for (x=1;x<=l;x++) {
-         acum = acum+`${m} `.yellow.underline+`X`.blue.underline+` ${x} `.red.underline+`=`.green.underline+` ${ m * x}\n`.rainbow.underline;
+         acumc = acumc+`${m} `.yellow.underline+`X`.blue.underline+` ${x} `.red.underline+`=`.green.underline+` ${ m * x}\n`.rainbow.underline;
+         acum = acum+`${m} X ${x} = ${ m * x}\n`;
     }
     if  (s) {
-        console.log (tit.green+acum)
+        console.log (tit.green+acumc)
     }
     return tit+acum
 }
